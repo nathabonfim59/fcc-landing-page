922d3006eefeb47a0ffe4f92c24a4ad9a2ef9eb9
nathabonfim59 <nathabonfim59@localhost.com>
2020-04-03 06:35:30 -0300

    Navbar dynamic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    checkNavbarToggle();
+    navbarToggleClicked();
+    navbarItemClicked();
 });
 
 /**
  * Checks if the navbar is clicked and show or hide it
  */
-function checkNavbarToggle() {
+function navbarToggleClicked() {
     var navbarToggleBtn = document.getElementsByClassName('navbar-toggle')[0];
 
     navbarToggleBtn.addEventListener("click", function() {
@@ -18,5 +19,28 @@ function checkNavbarToggle() {
             navbarNav.classList.remove('navbar-toggle-visible');
         }
 
+    });
+}
+
+/**
+ * Verifies if navbar is clicked and add the 'active' CSS class to it
+ */
+function navbarItemClicked() {
+    var navbarNav = document.getElementsByClassName('navbar-nav')[0];
+    navbarNav.addEventListener("click", function(event) {
+        var lastActive = document.getElementsByClassName('active')[0];
+        var targetElement;
+        
+        var anchorClicked = event.path.length == 8;
+        var listItemClicked = event.path.length == 7;
+
+        if (anchorClicked) {
+            targetElement = event.path[1];
+        } else if (listItemClicked) {
+            targetElement = event.path[0];
+        }
+
+        lastActive.classList.remove('active');
+        targetElement.classList.add('active');
     });
 }
